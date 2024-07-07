2fcdf0bff633ef618cb5bd48a1a815b10dfe8417
santo <santosgustavo431@gmail.com>
2024-07-07 15:26:23 -0300

    learning go

--- main/main.go
+++ main/main.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+	fmt.Println(cards.toString())
+	cards.saveToFile("cards")
+	cardFromfile := newDeckFromFile("cards")
+
+	cardFromfile.shuffle()
+	cardFromfile.print()
 }
